ec7708cff8cffc0bf811881d8c17761edb02f968
Somireddy.Jonnala <Somireddy.Jonnala@HYDHTC213262L.ad.infosys.com>
2018-04-28 17:05:50 +0200

    First project commit

---
+++ src/main/java/com/test/spboot/TestSp/Controller/MpAdapterController.java
@@ -0,0 +1,26 @@
+package com.test.spboot.TestSp.Controller;
+
+import java.io.IOException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/test")
+public class MpAdapterController {
+
+	
+
+
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public ResponseEntity<String> syncMetadataFromMP() throws IOException {
+
+		return new ResponseEntity<>("Hello! This is test Springboot appliation-Somi J.", HttpStatus.OK);
+	}
+
+
+}
---
+++ src/main/java/com/test/spboot/TestSp/TestSpApplication.java
@@ -0,0 +1,12 @@
+package com.test.spboot.TestSp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpApplication.class, args);
+	}
+}
