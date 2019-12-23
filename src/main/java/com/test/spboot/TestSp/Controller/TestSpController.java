6cb20be4f049e7f33c6d0d0fe5728266e8fab51e
Somireddy.Jonnala <Somireddy.Jonnala@HYDHTC213262L.ad.infosys.com>
2019-12-23 18:32:47 +0100

    changed code to add dockerfile

--- src/main/java/com/test/spboot/TestSp/Controller/TestSpController.java
+++ src/main/java/com/test/spboot/TestSp/Controller/TestSpController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
-public class MpAdapterController {
+public class TestSpController {
 
 	
 
